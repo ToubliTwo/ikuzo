e44dade0cb876452b2210b5fdc6eb5d06df5fe00
Carfaxx <lemaire.maud.pro@gmail.com>
2024-04-24 17:37:02 +0200

    Ajout Fixture de Lieu

--- src/Form/RechercheSortieFormType.php
+++ src/Form/RechercheSortieFormType.php
@@ -6,6 +6,8 @@ use App\Entity\Campus;
 use App\Entity\Sorties;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,8 +22,22 @@ class RechercheSortieFormType extends AbstractType
                 'class' => Campus::class,
                 'choice_label' => 'nom',
                 'placeholder' => 'Sélectionnez un campus',
-
             ])
+            ->add('dateSelectionnee', DateType::class, [
+                'widget' => 'single_text',
+                // Vous pouvez ajouter d'autres options de personnalisation ici
+            ])
+            // Ajout de la case à cocher pour les sorties dont vous êtes l'organisateur/trice
+            ->add('organisateur', CheckboxType::class, [
+                'label' => 'Sorties dont je suis l\'organisateur/trice',
+                'required' => false,
+            ])
+            // Ajout de la case à cocher pour les sorties auxquelles vous êtes inscrit/e
+            ->add('inscrit', CheckboxType::class, [
+                'label' => 'Sorties auxquelles je suis inscrit/e',
+                'required' => false,
+            ])
+
             ->add("submit", SubmitType::class, [
                 'label' => 'Rechercher'
             ])
