f78df2d6d2f16d6e9f19b11aba8a4980e470f9fb
ToubliDev <158035564+ToubliTwo@users.noreply.github.com>
2024-04-25 14:42:15 +0200

    Update AjouterSortieType.php

--- src/Form/AjouterSortieType.php
+++ src/Form/AjouterSortieType.php
@@ -15,7 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AjouterSortieType extends AbstractType
+class AjouterSortieFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
