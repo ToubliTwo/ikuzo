92c24c17ad4bff7222ce31e2ff41d910b0d036dd
DEVMJ00 <dev.mj@hotmail.com>
2024-05-02 16:48:31 +0200

    Les news du jeudi : - un peu de css

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -27,23 +27,23 @@ class ChangePasswordFormType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please enter a password',
+                            'message' => 'Merci d\'entrer un mot de passe',
                         ]),
                         new Length([
                             'min' => 12,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
                         new PasswordStrength(),
                         new NotCompromisedPassword(),
                     ],
-                    'label' => 'New password',
+                    'label' => 'Saisissez votre nouveau mot de passe :',
                 ],
                 'second_options' => [
-                    'label' => 'Repeat Password',
+                    'label' => 'Confirmez la saisie :',
                 ],
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Les champs ne correspondent pas, veuillez réessayer.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
